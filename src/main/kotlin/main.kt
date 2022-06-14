5b8da22ebb4c700913b68a4f63ab257208022972
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-13 23:21:03 -0300

    Add associate

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,103 +1,28 @@
 fun main() {
-    val orders = mutableMapOf(
-        Pair(3, 15.0),
-        1 to 20.0,
-        2 to 34.0
-    )
-
-//    val orderValue = orders.getValue(5)
-//    println(orderValue)
-
-    val message = orders.getOrElse(5) {
-        "Order not found!"
-    }
-    println(message)
-
-    val message2 = orders.getOrDefault(5, -1.0)
-    println(message2)
-
-    println(orders.keys)
-    println(orders.values)
-
-    for (number in orders.keys) {
-        println("Order $number")
-    }
-
-    for (value in orders.values) {
-        println("Order $value")
-    }
-
-    val filteredOrders = orders.filter { (number, value) ->
-        number % 2 == 0 && value > 15
-    }
-
-    println("Filtered orders: $filteredOrders")
-
-    println(orders + Pair(7, 90.0))
-    println(orders - 7)
-    println(orders - listOf(2, 1))
-
-    //orders.putAll(setOf(7 to 90.0, 8 to 20.0))
-    orders += setOf(7 to 90.0, 8 to 20.0)
-    println(orders)
-
-    orders.remove(3)
-    println(orders)
-
-    orders.values.remove(70.0)
-    println(orders)
-
-    orders.values.remove(20.0)
-    println(orders)
-
-    orders -= 8
-    println(orders)
-}
-
-fun testMap() {
-    //    mapOf(
-//        Pair(1, 20.0),
-//        Pair(2, 34.0)
-//    )
-
-    // Tem perda de performance, se precisar de performance use o Pair
-    val orders = mutableMapOf(
-        Pair(3, 15.0),
-        1 to 20.0,
-        2 to 34.0
+    val orders = listOf(
+        Order(1, 20.0),
+        Order(2, 30.0),
+        Order(3, 15.0),
+        Order(4, 10.0)
     )
 
     println(orders)
 
-    val order = orders[1]
-    order?.let {
-        println("Order: $it")
+//    val mappedOrders = orders.associate { order: Order ->
+//        Pair(order.number, order)
+//    }
+
+    val mappedOrders = orders.associateBy { order -> order.number }
+    println(mappedOrders)
+
+    val ordersFreeShipping = orders.associateWith { order ->
+        order.value > 20.0
     }
-
-    for (orderLoop in orders) {
-        println("Order number ${orderLoop.key}")
-        println("Order value ${orderLoop.value}")
-    }
-
-    orders[4] = 74.0
-    println(orders)
-
-    orders.put(5, 80.0)
-    println(orders)
-
-    orders.put(1, 100.0)
-    println(orders)
-
-    orders.putIfAbsent(6, 200.0)
-    println(orders)
-
-    orders.putIfAbsent(6, 100.0)
-    println(orders)
-
-    orders.remove(6)
-    println(orders)
-
-    orders.remove(3, 16.0)
-    println(orders)
+    println(ordersFreeShipping)
 }
+
+data class Order(
+    val number: Int,
+    val value: Double
+)
 
