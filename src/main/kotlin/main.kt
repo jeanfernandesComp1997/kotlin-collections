87db4b8fea1634004387a91bac43b95233f0306a
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-13 22:33:06 -0300

    Add Map

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,14 @@
 fun main() {
-    testSet()
+//    mapOf(
+//        Pair(1, 20.0),
+//        Pair(2, 34.0)
+//    )
+
+    val orders = mapOf(
+        1 to 20.0,
+        2 to 34.0
+    )
+
+    println(orders)
 }
 
