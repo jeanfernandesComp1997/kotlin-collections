0f9bee9aa571940658ec0000ba4b0ba4cc986bd8
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-13 22:46:44 -0300

    Test behavior

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,7 +5,8 @@ fun main() {
 //    )
 
     // Tem perda de performance, se precisar de performance use o Pair
-    val orders = mapOf(
+    val orders = mutableMapOf(
+        Pair(3, 15.0),
         1 to 20.0,
         2 to 34.0
     )
@@ -17,10 +18,30 @@ fun main() {
         println("Order: $it")
     }
 
-    for (order in orders) {
-        println("Order number ${order.key}")
-        println("Order value ${order.value}")
-
+    for (orderLoop in orders) {
+        println("Order number ${orderLoop.key}")
+        println("Order value ${orderLoop.value}")
     }
+
+    orders[4] = 74.0
+    println(orders)
+
+    orders.put(5, 80.0)
+    println(orders)
+
+    orders.put(1, 100.0)
+    println(orders)
+
+    orders.putIfAbsent(6, 200.0)
+    println(orders)
+
+    orders.putIfAbsent(6, 100.0)
+    println(orders)
+
+    orders.remove(6)
+    println(orders)
+
+    orders.remove(3, 16.0)
+    println(orders)
 }
 
