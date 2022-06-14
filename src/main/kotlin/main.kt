6368839befffb908c0ddc7fb10b28eeb2c148c1d
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2022-06-13 22:38:54 -0300

    Using map

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,11 +4,23 @@ fun main() {
 //        Pair(2, 34.0)
 //    )
 
+    // Tem perda de performance, se precisar de performance use o Pair
     val orders = mapOf(
         1 to 20.0,
         2 to 34.0
     )
 
     println(orders)
+
+    val order = orders[1]
+    order?.let {
+        println("Order: $it")
+    }
+
+    for (order in orders) {
+        println("Order number ${order.key}")
+        println("Order value ${order.value}")
+
+    }
 }
 
